0020e6ddec3c420f21c92c5fdf568b0f97199021
acarnilsu <nilsuacar42@gmail.com>
2023-01-03 21:28:20 +0300

    Add project files.

---
+++ IEA_SignalR_Api/Hubs/MyHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IEA_SignalR_Api.Hubs
+{
+    public class MyHub:Hub
+    {
+        public static List<string> Names { get; set; } = new List<string>();
+        public async Task SendName(string name)
+        {
+            Names.Add(name);
+            await Clients.All.SendAsync("ReceiveName", name);
+        }
+        public async Task GetNames()
+        {
+            await Clients.All.SendAsync("ReceiveNames", Names);
+        }
+    }
+}
